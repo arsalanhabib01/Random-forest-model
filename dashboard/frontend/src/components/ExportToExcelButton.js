90ba8ca7a6839ff2ec6ce84afd41f93ba7b1113b
Arsalan Habib <71399565+arsalanhabib01@users.noreply.github.com>
2023-05-16 10:44:25 +0200

    Updated comments to provide for better understanding of the code

--- dashboard/frontend/src/components/ExportToExcelButton.js
+++ dashboard/frontend/src/components/ExportToExcelButton.js
@@ -6,9 +6,10 @@ import { utils as XLSXUtils, writeFile as writeExcelFile } from 'xlsx';
 const ExportToExcelButton = ({ predictions }) => {
   const exportToExcel = () => {
     const currentDate = new Date();
-    const formattedDate = currentDate.toISOString().slice(0, 10);
-    const dataByDate = {};
+    const formattedDate = currentDate.toISOString().slice(0, 10); // Get the current date in the format 'YYYY-MM-DD'
+    const dataByDate = {}; // Store predictions data by date
 
+    // Group predictions data by date
     predictions.forEach((prediction) => {
       const date = new Date(prediction.date).toISOString().slice(0, 10);
       if (!dataByDate[date]) {
@@ -17,6 +18,7 @@ const ExportToExcelButton = ({ predictions }) => {
       dataByDate[date].push(prediction);
     });
 
+    // Create the Excel workbook and worksheet
     const workbook = XLSXUtils.book_new();
 
     let iterator = 0;
@@ -28,11 +30,13 @@ const ExportToExcelButton = ({ predictions }) => {
       }
     });
 
+    // When their is no prediction data is available for the current date
     if (iterator === 0) {
       alert('No predictions data available.');
       return;
     }
 
+    // Save the workbook as an Excel file with the current date
     writeExcelFile(workbook, `predictions_${formattedDate}.xlsx`);
   };
 
